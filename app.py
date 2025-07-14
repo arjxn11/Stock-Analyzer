c5fede2362abb428c92abbbe741acf5b016fcc1d
arjxn11 <arjxn11@gmail.com>
2025-07-15 00:11:13 +0800

    update

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ if st.button("Analyze"):
         if df[plot_cols].notna().any().all():
             st.subheader("📉 Price Chart")
             st.line_chart(df.set_index("Date")[plot_cols])
-            st.text("If VWAP<TWAP, then we can infer that more volume was traded at lower prices. \n"" VWAP is often used by institutions to evaluate trading efficiency, while TWAP is often used for execution algorithms to slice orders evenly over time")
+            st.markdown("If VWAP<TWAP, then we can infer that more volume was traded at lower prices. \n"" VWAP is often used by institutions to evaluate trading efficiency, while TWAP is often used for execution algorithms to slice orders evenly over time")
         else:
             st.warning("⚠️ One of Close/VWAP/TWAP is all NaN – cannot plot.")
 
