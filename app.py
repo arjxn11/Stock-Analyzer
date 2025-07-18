cc671d9b76f775f9a7cb07e3cf35a2e0ed58cf9f
arjxn11 <arjxn11@gmail.com>
2025-07-18 23:42:50 +0800

    sentiment model

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import torch
 import torch.nn.functional as F
 
 st.set_page_config(page_title="Stock Analyzer", layout="wide")
-st.title("📊 Stock Analyzer (Quantitative and Sentiments)")
+st.title("📊 Stock Analyzer (Quantitative, Sentiments, and Time Series Forecasting")
 
 # Request and receive user inputs
 tkr = st.text_input("Enter stock ticker (e.g., AAPL, NVDA):").upper().strip()
@@ -277,7 +277,8 @@ if st.button("Stock Analysis"):
         """)
 
 # Price Forecast
-
+if st.button("📊Price Forecast"):
+    st.markdown("This Model is currently being constructed. We are working on getting this live and running for your use ASAP! Thank you for your patience!")
 
 # Sentiment Analysis
 
